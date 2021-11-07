c64930e54aa75743e3051a10154882862a740b76
Y A S H <87377992+Bcoderx6@users.noreply.github.com>
2021-11-07 09:39:11 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,12 +45,15 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("REPO", "https://github.com/Bcoderx6/slam-mirrorbot")
+    buttons.buildbutton("OWNER", "https://t.me/YASHPUTHA")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
+🔑THIS BOT CAN MIRROR ALL YOUR LINKS TO GOOGLE DRIVE!😎
+
+🧨BOT AND REPO OWNER@YASHPUTHA🎯
+
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update, reply_markup)
